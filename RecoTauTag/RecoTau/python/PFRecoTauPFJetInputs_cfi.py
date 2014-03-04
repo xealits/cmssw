22cfe8e37c992b8364eeee8ec97f798718189e30
Pavel Jez <pavel.jez@cern.ch>
2014-03-04 13:58:07 +0100

    modified default filtering parameters

--- RecoTauTag/RecoTau/python/PFRecoTauPFJetInputs_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauPFJetInputs_cfi.py
@@ -5,7 +5,7 @@ from CommonTools.RecoAlgos.pfJetSelector_cfi import pfJetSelector
 tauInputJets = pfJetSelector.clone(
     src = cms.InputTag("ak5PFJets"),
     filter = cms.bool( False ),
-    cut = cms.string ( "pt > 15 && abs(eta) < 3.0")
+    cut = cms.string ( "pt > 14.5 && abs(eta) < 3.0")
 )
 
 PFRecoTauPFJetInputs = cms.PSet (
