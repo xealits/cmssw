49fe0a5f0565568a745a7dbafabd428d1dbc132a
Pavel Jez <pavel.jez@cern.ch>
2014-03-05 14:22:09 +0100

    added discriminators with Rho correction

--- RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByIsolation_cfi.py
+++ RecoTauTag/RecoTau/python/PFRecoTauDiscriminationByIsolation_cfi.py
@@ -49,7 +49,7 @@ pfRecoTauDiscriminationByIsolation = cms.EDProducer("PFRecoTauDiscriminationByIs
 
     # Rho corrections
     applyRhoCorrection = cms.bool(False),
-    rhoProducer = cms.InputTag("kt6PFJets", "rho"),
+    rhoProducer = cms.InputTag("fixedGridRhoFastjetAll"),
     rhoConeSize = cms.double(0.5),
     rhoUEOffsetCorrection = cms.double(1.0)
 )
