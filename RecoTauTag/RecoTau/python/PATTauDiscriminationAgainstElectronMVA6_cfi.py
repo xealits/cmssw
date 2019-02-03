59b57a8f32416331e195a42206c861e983d74e10
swozniewski <sebastian.wozniewski@kit.edu>
2019-02-03 19:33:41 +0100

    restore vetoEcalCracks = cms.bool(True) removed erroneously during rebasing

--- RecoTauTag/RecoTau/python/PATTauDiscriminationAgainstElectronMVA6_cfi.py
+++ RecoTauTag/RecoTau/python/PATTauDiscriminationAgainstElectronMVA6_cfi.py
@@ -34,6 +34,7 @@ patTauDiscriminationAgainstElectronMVA6 = cms.EDProducer("PATTauDiscriminationAg
     minMVAWgWgsfEC             = cms.double(0.0),
 
     srcElectrons = cms.InputTag('slimmedElectrons'),
+    vetoEcalCracks = cms.bool(True),
     usePhiAtEcalEntranceExtrapolation = cms.bool(False),
     verbosity = cms.int32(0)
 )
